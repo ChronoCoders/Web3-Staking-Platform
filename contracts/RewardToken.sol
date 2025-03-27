32a93c6be4b40745a8e664fbc8ffdcac5f08c0a4
ChronoCoders <altug@bytus.io>
2025-03-27 17:40:25 -0500

    Add smart contract implementation and deployment scripts

---
+++ contracts/RewardToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RewardToken is ERC20, Ownable {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        // Mint initial supply to the contract creator
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+    
+    // Mint additional tokens (only owner)
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
